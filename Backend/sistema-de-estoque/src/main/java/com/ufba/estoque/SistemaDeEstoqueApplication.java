df8b2fb44271153a7f8501546da136c9e6927320
Marcos Vinicius <mdominique2011@gmail.com>
2023-10-04 17:46:34 -0300

    FEATURE, adicionando ambiente do backend

---
+++ Backend/sistema-de-estoque/src/main/java/com/ufba/estoque/SistemaDeEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.ufba.estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeEstoqueApplication.class, args);
+	}
+
+}
