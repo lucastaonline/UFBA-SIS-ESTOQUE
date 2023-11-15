d11142e8bf3d376739ff82c584e7f5a11ab7e781
anderson <loyolaandersom2013@gmail.com>
2023-11-15 19:08:22 -0300

    enable cors

--- backend/stock_control/src/main/java/com/ufba/stock_control/controllers/UsersController.java
+++ backend/stock_control/src/main/java/com/ufba/stock_control/controllers/UsersController.java
@@ -6,6 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +28,7 @@ import jakarta.validation.Valid;
   consumes = MediaType.APPLICATION_JSON_VALUE,
   produces = MediaType.APPLICATION_JSON_VALUE
 )
+@CrossOrigin(origins = "*")
 public class UsersController {
     
     @Autowired
