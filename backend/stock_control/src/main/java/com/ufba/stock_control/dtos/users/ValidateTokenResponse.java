4ffd3b62d0633fd66a7643dec10ec82b386ca362
anderson <loyolaandersom2013@gmail.com>
2023-11-15 21:54:21 -0300

    adds token validation route

--- backend/stock_control/src/main/java/com/ufba/stock_control/dtos/users/ValidateTokenResponse.java
+++ backend/stock_control/src/main/java/com/ufba/stock_control/dtos/users/ValidateTokenResponse.java
@@ -1,16 +1,14 @@
 package com.ufba.stock_control.dtos.users;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
-@NoArgsConstructor
 @AllArgsConstructor
-public class LoginResponse {
+@NoArgsConstructor
+@Builder
+public class ValidateTokenResponse {
   String message;
-  String token;
 }
